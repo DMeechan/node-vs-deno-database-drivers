c61c2c3eb597d975c0ed80046d733cff216433ad
Daniel Meechan <git@public.dmeechan.com>
2023-08-17 01:01:36 +0100

    refactor: use common import names across Node and Deno

--- src/deno.ts
+++ src/deno.ts
@@ -2,14 +2,14 @@ import "https://deno.land/std@0.198.0/dotenv/load.ts";
 
 // Need to use a fork because "https://deno.land/x/mysql/mod.ts" doesn't support TLS yet.
 // Waiting for https://github.com/denodrivers/mysql/pull/154 to be merged.
-import { Client } from "https://raw.githubusercontent.com/shiyuhang0/mysql/tls/mod.ts";
-import { connect } from "npm:@planetscale/database@1.10.0";
+import { Client } from "shiyuhang0/mysql/tls";
+import { connect } from "@planetscale/database";
 
 import { planetscaleServerless } from "./drivers/planetscale-serverless.ts";
 import { denoMysql } from "./drivers/deno_mysql.ts";
 import { run } from "./run.ts";
 
-// import { createConnection } from "npm:mysql2@^3.6.0/promise";
+// import { createConnection } from "mysql2/promise";
 // import { mysql2 } from "./drivers/mysql2.ts";
 
 async function init() {
